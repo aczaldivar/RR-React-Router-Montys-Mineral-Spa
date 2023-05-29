9974790c1a40ec1da5df0f5239d84045c489f4b1
Anna Zaldivar <anna.zaldivar@gmail.com>
2023-05-29 12:08:46 -0700

    shows info

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,12 +1,15 @@
 
 
-export default function Packages() {
+export default function Packages(props) {
 
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
+                    <ul>
+                    {props.packages.map((element,i )=> <p style={{fontSize:"20px"}} key ={i}>{element} </p>)}
+                </ul>
                 </div>
                 
             </div>
